55caa2f992487818de63506e12797d6ce81f8701
justBri <brirhammond@gmail.om>
2025-01-27 13:40:03 -0800

    use json file as fake db"

--- src/resources.ts
+++ src/resources.ts
@@ -1,43 +1,22 @@
-const fakeDb = {
-    people: [
-        {
-            firstName: 'Jane',
-            lastName: 'Doe',
-            fullName: 'Jane Doe'
-        },
-        {
-            firstName: 'Jane',
-            lastName: 'Doe',
-            fullName: 'Jane Doe'
-        },
-        {
-            firstName: 'Jane',
-            lastName: 'Doe',
-            fullName: 'Jane Doe'
-        },
-        {
-            firstName: 'Jane',
-            lastName: 'Doe',
-            fullName: 'Jane Doe'
-        },
-        {
-            firstName: 'Jane',
-            lastName: 'Doe',
-            fullName: 'Jane Doe'
-        },
-        {
-            firstName: 'Jane',
-            lastName: 'Doe',
-            fullName: 'Jane Doe'
-        },
-    ]
+import { readFileSync } from 'fs';
+
+type Person = {
+    firstName: string;
+    lastName: string;
+    fullName: string;
+};
+
+interface Fakedb {
+    people: Person[];
 }
+
+const db: Fakedb = JSON.parse(readFileSync('./src/fakedb.json', 'utf-8'))
 
 export const getPeople = (version: string) => {
     switch (version) {
         case 'v1':
-            return fakeDb.people.map(({ fullName, ...rest }) => rest);
+            return db.people.map(({ fullName, ...rest }) => rest);
         default:
-            return fakeDb.people.map(({ fullName }) => ({ fullName }));
+            return db.people.map(({ fullName }) => ({ fullName }));
     }
 }
