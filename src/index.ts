994c009fa0196e9ae45013b408a00038f4c4fcb5
justBri <brirhammond@gmail.om>
2025-01-30 15:00:01 -0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
-import express from 'express';
-import { getPeople } from './resources';
+import express from "express";
+import { getPeople } from "./resources";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+app.get("/api/:version/people", (req, res) => {
+  // pull out version from route
+  const { version } = req.params;
 
-app.get('/api/:version/people', (req, res) => {
-    // pull out version from route
-    const { version } = req.params;
-
-    const data = getPeople(version)
-    res.send({ people: data })
-
+  const data = getPeople(version);
+  res.send({ people: data });
 });
 
 app.listen(port, () => {
-    console.log(`App up and running on port ${port}`)
-})
+  console.log(`App up and running on port ${port}`);
+});
--- src/resources.ts
+++ src/resources.ts
@@ -1,22 +1,26 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
 type Person = {
-    firstName: string;
-    lastName: string;
-    fullName: string;
+  firstName: string;
+  lastName: string;
+  fullName: string;
 };
 
 interface Fakedb {
-    people: Person[];
+  people: Person[];
 }
 
-const db: Fakedb = JSON.parse(readFileSync('./src/fakedb.json', 'utf-8'))
+const db: Fakedb = JSON.parse(readFileSync("./src/fakedb.json", "utf-8"));
 
 export const getPeople = (version: string) => {
-    switch (version) {
-        case 'v1':
-            return db.people.map(({ fullName, ...rest }) => rest);
-        default:
-            return db.people.map(({ fullName }) => ({ fullName }));
-    }
-}
+  switch (version) {
+    case "v1":
+      return db.people.map(({ fullName, ...rest }) => rest);
+    case "v2":
+      return db.people.map(({ fullName }) => ({ fullName }));
+    default:
+      // Added warning for invalid version
+      console.warn(`Unsupported API version: ${version}`);
+      throw new Error("Invalid API version");
+  }
+};
